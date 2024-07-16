b48b392186762c861df2f273e027c266a9030b57
VeraArk <veraarkind@gmail.com>
2024-07-16 15:41:58 +0300

    add Homework11

--- src/App.tsx
+++ src/App.tsx
@@ -6,43 +6,12 @@ import Homework06 from "./homework/Homework06/Homework06";
 import Homework08 from "homework/Homework08/Homework08";
 import Homework09 from "homework/Homework09/Homework09";
 import Homework10 from "homework/Homework10/Homework10";
-
-import Lesson06 from "./lessons/Lesson06/Lesson06";
-import Lesson07 from "./lessons/Lesson07/Lesson07";
-import Lesson08 from "lessons/Lesson08/Lesson08";
-
-
-function App() {
-  return (
-    <>
-      <GlobalStyles />
-      {/*< Homework06/>*/}
-      {/*<Homework07 />*/}
-       {/*<Homework08 />*/}
-        {/*<Homework09 />*/}
-      
-      {/*<Lesson07/>*/}
-      {/*<Lesson08 />*/}
-
-      <Homework10/>
-    </>
-  );
-}
-
-export default App;
-
-//import {AppWrapper} from "./appStyles";
-import GlobalStyles from "styles/GlobalStyles";
-
-import Homework07 from "homework/Homework07/Homework07";
-import Homework06 from "./homework/Homework06/Homework06";
-import Homework08 from "homework/Homework08/Homework08";
+import Homework11 from "homework/Homework11/Homework11";
 
 
 import Lesson06 from "./lessons/Lesson06/Lesson06";
 import Lesson07 from "./lessons/Lesson07/Lesson07";
 import Lesson08 from "lessons/Lesson08/Lesson08";
-import Lesson_11 from "lessons/Lesson11/Lesson11";
 
 function App() {
   return (
@@ -51,11 +20,10 @@ function App() {
       {/*< Homework06/>*/}
       {/*<Homework07 />*/}
       {/*<Homework08 />*/}
+      {/*<Homework09 />*/}
 
       {/*<Lesson07/>*/}
       {/*<Lesson08 />*/}
-      {/*<Lesson09 />*/}
-      {/*<Lesson11 />*/}
 
       <Homework11 />
     </>
---
+++ src/homework/Homework11/Homework11.tsx
@@ -0,0 +1,15 @@
+import { PageWrapper } from "homework/Homework07/styles";
+
+import ContactUs from "components/ContactUs/ContactUs";
+
+function Homework11(){
+    return (<PageWrapper>
+    <ContactUs/>
+    </PageWrapper>)
+}
+
+export default Homework11;
+
+
+
+
